00ff2e7aad89113d9bcb997a0b5cc0530b7eda2c
TwoEther <lhi0711@naver.com>
2024-02-24 10:13:27 +0900

    profile_config추가

--- src/main/java/org/project/ShopApplication.java
+++ src/main/java/org/project/ShopApplication.java
@@ -1,13 +1,29 @@
 package org.project;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Enumeration;
+import java.util.Properties;
+
+@Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class ShopApplication {
 	public static void main(String[] args) {
+		System.setProperty("spring.profiles.default", "prod");
+		Properties p = System.getProperties();
+		Enumeration keys = p.keys();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			String value = (String) p.get(key);
+			System.out.println(key + ": " + value);
+		}
+
 		SpringApplication.run(ShopApplication.class, args);
 	}
 
